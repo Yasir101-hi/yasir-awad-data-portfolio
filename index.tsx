c4ae8a106d492946e881929bdaa6ac5b018aa378
Yasir Awad <yasir.m.ahmed23@gmail.com>
2026-08-30 07:04:02 +0300

    Route resume page to approved concise CV content

--- index.tsx
+++ index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Resume from './Resume';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
+const isResumePage = window.location.pathname.replace(/\/+$/, '') === '/resume';
+
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    {isResumePage ? <Resume /> : <App />}
   </React.StrictMode>
 );
